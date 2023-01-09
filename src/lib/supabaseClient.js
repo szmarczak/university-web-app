e3965698255efc9c49314e447347f845d362ddb1
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2023-01-09 08:46:39 +0100

    env

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,4 +1,7 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit'
 import { env } from '$env/dynamic/public'
 
+env.PUBLIC_SUPABASE_URL="https://sryawqipfxjmnutudvuz.supabase.co"
+env.PUBLIC_SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNyeWF3cWlwZnhqbW51dHVkdnV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzMyNDEyOTcsImV4cCI6MTk4ODgxNzI5N30.neKb86VB4i87DrNFRcCXtORP0q-RFjg1AOCIop10Jo8"
+
 export const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
